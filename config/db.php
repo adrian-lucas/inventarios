d35454a3575504c8d7cb63d3460f6940278bb5e8
adrian-lucas <donplucas@gmail.com>
2023-01-24 11:44:38 -0400

    metodos agregados, aun falta el manejo de errores

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=inventario',
+    'dsn' => 'pgsql:host=localhost;dbname=inventarios',
     'username' => 'postgres',
     'password' => 'postgres',
     'charset' => 'utf8',
--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 use Yii;
 use app\models\Producto;
+use app\models\ProductoCategoria;
 use yii\data\ActiveDataProvider;
 use yii\base\ErrorException;
 use yii\db\IntegrityException;
@@ -17,8 +18,16 @@ class ProductoController extends \yii\web\Controller
         'class' => \yii\filters\VerbFilter::class,
         'actions' => ['register'=>['post'],
                       'view-all'=>['get'],
-                      'viewPagination' =>['get'],
+                      'view-one'=>['get'],
+                      'view-pagination' =>['get'],
                       'remove'=>['delete','get'],
+                      'change-data'=>['put'],
+                      'view-products-by-section'=>['get'],
+                      'total-stock-by-mark'=>['get'],
+                      'maximum-stock'=>['get'],
+                      'is-more-than-zeso'=>['get'],
+                      'un-linked'=>['delete'],
+                      'linked'=>['post'],
                      ]
      ];
      return $behaviors;
@@ -48,9 +57,14 @@ class ProductoController extends \yii\web\Controller
     public function actionRemove()
     {   
         $body = Yii::$app->getRequest()->getBodyParam('id');
-        $producto = Producto::findOne($body)->delete();
-       
-        return $producto;
+        $producto = Producto::findOne($body);
+        
+        
+        try{
+            $producto->delete();
+        }catch(Exception $e){
+            return ['succes'=> false,'message'=>'Error en el registro','Error'=>$producto->errors];
+        }       
     }
     public function actionViewOne()
     {
@@ -104,13 +118,66 @@ class ProductoController extends \yii\web\Controller
     {
         $idMark = Yii::$app->getRequest()->getBodyParam('id');
         $totalStock = Producto::find()
-                            ->select(['producto.nombre as nombreproducto','producto.marca_id'])
-                            
+                            ->select(['SUM(producto.stock) as stockToral'])
+                            ->leftJoin('marca','producto.marca_id=marca.id')
                             ->where(['producto.marca_id'=>$idMark])
+                            ->groupBy('producto.marca_id')
                             ->asArray()
                             ->all();
         return $totalStock;
     }
-   
 
+    public function actionMaximumStock()
+    {
+        return Producto::find()
+                            ->select(['MAX(stock)'])
+                            ->asArray()
+                            ->all();                 
+    }
+    public function actionIsMoreThanZero()
+    {
+        $res = false;
+        $idProducto = Yii::$app->getRequest()->getBodyParam('id');
+        $query = Producto::find()
+                            ->select(['stock'])
+                            ->where(['id'=>$idProducto])
+                            ->one();
+        $stock = $query->stock;
+        if($stock>0)
+        {
+            $res = true;
+        }
+        return ['Stock is more than zero'=>$res];                                                                                         
+    }
+    public function actionUnLinked()
+    {
+        $idCategoria = Yii::$app->getRequest()->getBodyParam('idCategoria');
+        $idProducto = Yii::$app->getRequest()->getBodyParam('idProducto');
+        return ProductoCategoria::deleteAll(['categoria_id'=>$idCategoria,'producto_id'=>$idProducto]);
+                                /*
+                                ->where(['categoria_id'=>$idCategoria,'producto_id'=>$idProducto])
+                                ->asArray()
+                                ->all();
+                                */
+    }
+    public function actionUnLinkedByBatch()
+    {
+        $idCategoria = Yii::$app->getRequest()->getBodyParam('idCategoria');
+        
+        return ProductoCategoria::find()
+                                ->where(['categoria_id'=>$idCategoria])
+                                ->asArray()
+                                ->all();
+
+    }
+    public function actionLinked()
+    {
+        $body = Yii::$app->getRequest()->getBodyParams();
+        $productoCategoria = new ProductoCategoria();
+        $productoCategoria->load($body,'');
+        $productoCategoria->save();
+        return $productoCategoria;  
+
+    }
+     
 }
